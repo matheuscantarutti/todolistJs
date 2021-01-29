7d6893621c0661453791e75861d6fcb087c8beb7
MatheusABC <matheuscantarutti@gmail.com>
2021-01-29 18:23:34 -0300

    retirando funções do escopo global

--- taskList.js
+++ taskList.js
@@ -1,11 +1,13 @@
-const criarTarefa = (evento) => {
+(() => {
+    const criarTarefa = (evento) => {
     
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const valor = input.value;
-    console.log(valor);
-    input.value="";
-}
-
-const novaTarefa = document.querySelector("[data-form-btn]");
-novaTarefa.addEventListener("click", criarTarefa);
+        evento.preventDefault();
+        const input = document.querySelector("[data-form-input]");
+        const valor = input.value;
+        console.log(valor);
+        input.value="";
+    }
+    
+    const novaTarefa = document.querySelector("[data-form-btn]");
+    novaTarefa.addEventListener("click", criarTarefa);
+});
